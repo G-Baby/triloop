edc85e733d8ccc212d31152a12a21c110cd9aac1
G-Baby <gpw10097@saintjoe.edu>
2015-01-26 11:09:32 -0600

    Update triloop.js

--- triloop.js
+++ triloop.js
@@ -1,4 +1,5 @@
 //This is my triloop homework assignment
+//Gabriel Perez, Prints out a triangle of hashtags
 console.log("#");
 console.log("##");
 console.log("###");
@@ -6,3 +7,4 @@ console.log("####");
 console.log("#####");
 console.log("######");
 console.log("#######");
+//this code prints out a triangle of hash marks
